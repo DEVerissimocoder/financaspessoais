d7b55a21d3471f19687a183944d5771c340b93c1
DEVerissimocoder <danielverissimo1d@gmail.com>
2025-05-05 17:12:00 -0300

    organizando arquivos .json

--- finances.py
+++ finances.py
@@ -1,37 +1,33 @@
 import json
 
-
+lista=[]
 def createExpense():
     payment_method =    input("informe a forma de pagamento\n")
     person =            input("informe a pessoa devedora\n")
-    description =       input("informe uma descrição\n")
+    """description =       input("informe uma descrição\n")
     value =             float(input("informe o valor da despesa\n"))
     status =            input("informe o status\n")
-    modalidade =        input("informe a modalidade\n")
-
+    modalidade =        input("informe a modalidade\n")"""
 
     dict_expenses={
         "payment_method": payment_method,
-        "person"        : person,
-        "description"   : description,
-        "status"        : status,
-        "modalidade"    : modalidade
-    } 
+        "person"        : person        
+    }     
+    arquivo=json.load()
     with open('financespessoais.json', 'a', encoding='utf-8') as arquivo:
-        json.dump(dict_expenses, arquivo)
-        arquivo.write('\n')
-def updateExpense():
+        json.dump(dict_expenses, arquivo, ensure_ascii=False)
+        
+def listExpense():
     with open('financespessoais.json', 'r') as arquivo:
         conteudo = json.load(arquivo)
-    print('-------forma de pagamento\n---------')
-    print(f'{conteudo['payment_method']}')
-    print('----------\ndevedor\n----------')
-    print(conteudo['person'])
-    print('----------\ndescrição\n----------')
-    print(conteudo['description'])
-    print('----------\nstatus\n----------')
-    print(conteudo['status'])
-    print('----------\nmodalidade\n----------')
-    print(conteudo['modalidade'])
-        
-updateExpense()
+    print(f'''
+          ----------------  -------  ---------  --------  -----------------
+          |  FORMA_PAGAM     PESSOA   DESCRIÇÃO  STATUS    MODALIDADE     |
+          ----------------  -------  ---------  --------  -----------------
+          |      {conteudo['payment_method']}         {conteudo['person']}      {conteudo['description']}      {conteudo['status']}     {conteudo['modalidade']}   |
+          -----------------------------------------------------------------   
+''')
+    return conteudo
+   
+#listExpense()
+createExpense()
